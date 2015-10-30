ac4d381c1cfc9b6745ad27d30387b153d221fbbb
Rushaine McBean <rushaine.mcbean@gmail.com>
2015-10-30 17:28:21 -0400

    Adds archiving users and protects against self archiving

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,9 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-
   enum role: [ :user, :admin ]
+
+  scope :excluding_archived, lambda { where(archived_at: nil) }
 
   def is_admin?
   	 admin?
@@ -13,5 +14,9 @@ class User < ActiveRecord::Base
 
   def to_s
   	"#{email} (#{role.titleize})"
+  end
+
+  def archive
+  	self.update(archived_at: Time.now)
   end
 end

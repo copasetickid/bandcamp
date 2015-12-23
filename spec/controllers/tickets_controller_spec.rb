193d5bfa67f905c7594c03087030e805b39eee7e
Rushaine McBean <rushaine.mcbean@gmail.com>
2015-12-23 18:32:21 -0500

    Start of adding more tests to the tickets controller

--- spec/controllers/tickets_controller_spec.rb
+++ spec/controllers/tickets_controller_spec.rb
@@ -4,14 +4,35 @@ RSpec.describe TicketsController, type: :controller do
 	let(:project) { create(:project) }
 	let(:user) { create(:user) }
 
-	before :each do 
-		assign_role!(user, :editor, project)
-		sign_in user 
-	end
+	
 
-	it "can create tickets, but not tag them" do 
-		post :create, ticket: { name: "New ticket!", description: "Brand spankin' new", tag_names: "these are tags" },
-					project_id: project.id 
-		expect(Ticket.last.tags).to be_empty
+	describe "POST #create" do 
+		context "for editors" do 
+			before :each do 
+				assign_role!(user, :editor, project)
+				sign_in user 
+			end
+
+			it "can create tickets, but not tag them" do 
+				post :create, ticket: { name: "New ticket!", description: "Brand spankin' new", tag_names: "these are tags" },
+							project_id: project.id 
+				expect(Ticket.last.tags).to be_empty
+			end
+		end
+
+		context "for viewers" do 
+			before :each do 
+				assign_role!(user, :viewer, project)
+				sign_in user 
+			end
+
+			it "does not allow a ticket to be created" do 
+				post :create, ticket: { name: "New ticket!", description: "Brand spankin' new", tag_names: "these are tags" },
+							project_id: project.id 
+				expect(response.status).to eq 302
+			end
+		end
+
+
 	end
 end
